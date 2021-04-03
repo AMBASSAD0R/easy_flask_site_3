d3f004740f6f88df3c76d17aa978cc09e8957f00
GGwp-ctrl <for-games.04@yandex.ru>
2021-04-03 13:40:21 +0300

    initial commit

--- site1/server.py
+++ site1/server.py
@@ -133,24 +133,25 @@ def mars_carousel():
                   <head>
                     <meta charset="utf-8">
                     <title>Пейзажи Марса</title>
-                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-                  </head>
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+                </head>
                   <body>
                     <h1>Пейзажи Марса</h1>
-                    <div id="carousel" class="carousel slide" data-ride="carousel">
+                    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
                       <div class="carousel-inner">
                         <div class="carousel-item active">
-                          <img class="img-fluid" src="{url_for('static', filename='img/mars_1.jpg')}" alt="first slide">
+                          <img src="{url_for('static', filename='img/mars_1.jpg')}" alt="...">
                         </div>
                         <div class="carousel-item">
-                          <img class="img-fluid" src="{url_for('static', filename='img/mars_2.jpg')}" alt="second slide">
+                          <img src="{url_for('static', filename='img/mars_2.jpg')}" alt="...">
                         </div>
                         <div class="carousel-item">
-                          <img class="img-fluid" src="{url_for('static', filename='img/mars_3.jpg')}" alt="third slide">
+                          <img src="{url_for('static', filename='img/mars_3.jpg')}" alt="...">
                         </div>
                       </div>
                     </div>
-                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
                   </body>
                 </html>"""
 
